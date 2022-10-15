f3aae8d2259a2990390273bd0fbd006a50ef3f0c
jam-dev <jamalvga2002@gmail.com>
2022-10-16 05:22:15 +0530

    style changed

--- react-socketio-chat-app/client/src/components/container/Container.jsx
+++ react-socketio-chat-app/client/src/components/container/Container.jsx
@@ -38,12 +38,12 @@ class Container extends React.Component
                 <div class="tools-section">
                     <div className="color-picker-container">
                         Select Brush Color : &nbsp; 
-                        <input type="color" value={this.state.color} onChange={this.changeColor.bind(this)}/>
+                        <input id="selectcolor" type="color" value={this.state.color} onChange={this.changeColor.bind(this)}/>
                     </div>
 
                     <div className="brushsize-container">
                         Select Brush Size : &nbsp; 
-                        <select value={this.state.size} onChange={this.changeSize.bind(this)}>
+                        <select id="selectsize" value={this.state.size} onChange={this.changeSize.bind(this)}>
                             <option> 5 </option>
                             <option> 10 </option>
                             <option> 15 </option>
