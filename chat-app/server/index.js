a0e01fcd86c7c73aa8ef81cb6d29af48e63bb524
jam-dev <jamalvga2002@gmail.com>
2022-10-15 20:04:54 +0530

    server updated

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-// const { Server } = require("socket.io");
-// app.use(cors());
-
+const { Server } = require("socket.io");
 app.use(cors());
 
+
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection",(socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        consol.log("User disconned", socket.id)
+    })
+})
 
 server.listen(5001, () => {
     console.log("server running");
