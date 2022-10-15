8a75d63f511a16d2567cde31a5c715d7d72f8f29
jam-dev <jamalvga2002@gmail.com>
2022-10-15 21:07:52 +0530

    react updated

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
 
-
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -15,16 +14,23 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection",(socket) => {
-    console.log(socket.id);
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("disconnect", () => {
-        consol.log("User disconned", socket.id)
-    })
-})
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-server.listen(5001, () => {
-    console.log("server running");
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
-
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
