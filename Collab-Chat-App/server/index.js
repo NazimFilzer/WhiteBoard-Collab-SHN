f724b6a789da416d554974dbb4bfb5944d4af676
NzM <nazimfilzer@gmail.com>
2022-10-16 16:46:12 +0530

    COrs not there

--- Collab-Chat-App/server/index.js
+++ Collab-Chat-App/server/index.js
@@ -7,12 +7,7 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+
 
 io.on("connection", (socket) => {
 
